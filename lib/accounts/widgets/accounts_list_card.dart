f5abfe305e17ddf9d44de5fbe47710149702166e
Juan Alvarez <jlalvarez18@gmail.com>
2024-06-27 14:58:08 -0500

    fixed animation on account list widget
    - added visibility_detector package

--- lib/accounts/widgets/accounts_list_card.dart
+++ lib/accounts/widgets/accounts_list_card.dart
@@ -1,12 +1,10 @@
-import 'dart:async';
-
-import 'package:after_layout/after_layout.dart';
 import 'package:ava_light/accounts/models/account.dart';
 import 'package:ava_light/core/theme/colors.dart';
 import 'package:ava_light/core/ui/spaced_column.dart';
 import 'package:ava_light/core/widgets/card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class AccountsListCard extends StatelessWidget {
   const AccountsListCard({super.key, required this.accounts});
@@ -45,7 +43,7 @@ class AccountWidget extends StatefulWidget {
   State<AccountWidget> createState() => _AccountWidgetState();
 }
 
-class _AccountWidgetState extends State<AccountWidget> with AfterLayoutMixin {
+class _AccountWidgetState extends State<AccountWidget> {
   double progressValue = 0.0;
 
   @override
@@ -56,51 +54,55 @@ class _AccountWidgetState extends State<AccountWidget> with AfterLayoutMixin {
     final percentFormatter = NumberFormat.percentPattern();
     final dateFormatter = DateFormat.yMMMMd();
 
-    return SpacedColumn(
-      spacing: 12,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(widget.account.name, style: textTheme.titleLarge),
-            Text(percentFormatter.format(widget.account.usagePercentage),
-                style: textTheme.titleLarge),
-          ],
-        ),
-        TweenAnimationBuilder<double>(
-          tween: Tween(begin: 0, end: widget.account.usagePercentage),
-          duration: Duration(seconds: 1),
-          builder: (context, value, child) {
-            return LinearProgressIndicator(
-              value: value,
-              minHeight: 8,
-              borderRadius: BorderRadius.circular(999),
-              valueColor: AlwaysStoppedAnimation(AvaColors.green),
-            );
-          },
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(widget.account.balance.toString(),
-                style: textTheme.titleMedium),
-            Text("${widget.account.limit.toString()} Limit",
-                style: textTheme.titleMedium),
-          ],
-        ),
-        Text(
-          "Reported on ${dateFormatter.format(widget.account.dateLastReported)}",
-          style: textTheme.labelSmall,
-        ),
-      ],
+    return VisibilityDetector(
+      key: Key(widget.account.uid),
+      onVisibilityChanged: (info) {
+        final visiblePercentage = info.visibleFraction * 100;
+        if (visiblePercentage >= 90) {
+          setState(() {
+            progressValue = widget.account.usagePercentage;
+          });
+        }
+      },
+      child: SpacedColumn(
+        spacing: 12,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(widget.account.name, style: textTheme.titleLarge),
+              Text(percentFormatter.format(widget.account.usagePercentage),
+                  style: textTheme.titleLarge),
+            ],
+          ),
+          TweenAnimationBuilder<double>(
+            tween: Tween(begin: 0, end: progressValue),
+            duration: Duration(seconds: 1),
+            builder: (context, value, child) {
+              return LinearProgressIndicator(
+                value: value,
+                minHeight: 8,
+                borderRadius: BorderRadius.circular(999),
+                valueColor: AlwaysStoppedAnimation(AvaColors.green),
+              );
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(widget.account.balance.toString(),
+                  style: textTheme.titleMedium),
+              Text("${widget.account.limit.toString()} Limit",
+                  style: textTheme.titleMedium),
+            ],
+          ),
+          Text(
+            "Reported on ${dateFormatter.format(widget.account.dateLastReported)}",
+            style: textTheme.labelSmall,
+          ),
+        ],
+      ),
     );
-  }
-
-  @override
-  FutureOr<void> afterFirstLayout(BuildContext context) {
-    // setState(() {
-    //   progressValue = widget.account.usagePercentage;
-    // });
   }
 }
